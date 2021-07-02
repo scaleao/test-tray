40a5451d981a64b2cc2313b6f59de1321d14f4ac
João Scaleão <j.scaleao@hotmail.com>
2021-07-02 02:54:21 -0300

    feat: create migrations Vendas and Jobs

---
+++ teste-tray/database/migrations/2021_06_29_055501_create_vendas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->float('valor', 15, 2)->default('00.00');
+            $table->float('comissao', 15, 2)->default('00.00');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendas');
+    }
+}
